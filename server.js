3d2fa9ab4fe6727443f059fde8c8811e250e0ea2
victor <victor.eduardo.sntos@gmail.com>
2022-07-02 20:41:38 -0300

    configurando runner da aplicação em produção

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static('./src'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('src/index.html', {root: 'dist/app-heroku/'}),
+  res.sendFile('index.html', {root: './src'}),
 );
 
 // Start the app by listening on the default Heroku port
